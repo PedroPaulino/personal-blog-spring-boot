7372502c962ed104d2a1c0a5bd9ec5626a606e80
Pedro <pedropaulinoeng@hotmail.com>
2026-06-23 11:39:49 -0300

    Applied Bean Validation for ArticleUpdate Request

--- src/main/java/com/example/personal_blog/dto/ArticleUpdateRequest.java
+++ src/main/java/com/example/personal_blog/dto/ArticleUpdateRequest.java
@@ -1,5 +1,26 @@
 package com.example.personal_blog.dto;
 
-public record ArticleUpdateRequest(String title, String content, String category, String tags) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record ArticleUpdateRequest(
     
-}
+    @NotNull
+    @NotBlank(message = "Article Title must be filled")
+    String title, 
+    
+    @NotNull
+    @Size(
+        min = 240,
+        message = "Article Content must be higher than 240 chars."
+    )
+    String content, 
+    
+    @NotNull
+    @NotBlank(message = "Article Category must be filled.")
+    String category, 
+    
+    String tags
+
+) {}
